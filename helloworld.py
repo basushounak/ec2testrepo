093852f3d8b49326c941665674c59736871999ae
Ubuntu <ubuntu@ip-172-31-25-56.us-east-2.compute.internal>
2022-09-03 07:46:11 +0000

    Python demo/prac

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+def matrix(choice):
+	if choice == "a":
+		print("Hello World!")
+	elif choice == "b":
+		print("It was good knowing you")
+	else:
+		print("Wrong Choice")
+
+choice = input("Enter your choice a/b: ")
+matrix(choice)
